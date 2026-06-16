9cee3a9f40b29ef356b29dac3adc87b47d20a44b
KQD FIREWALL <inforajkumar1442@gmail.com>
2026-06-16 16:53:39 +0530

    vite config udpate

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-const apiUrl = import.meta.env.VITE_API_URL;
 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target:   apiUrl || "",
-        changeOrigin: true,
-      },
-      '/uploads': {
-        target: apiUrl || "",
-        changeOrigin: true,
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+        '/uploads': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
